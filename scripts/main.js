dc90158f8cba13001682f8f7db6a20c30be7b684
Ashish <yesashishs@gmail.com>
2020-12-25 21:01:30 -0500

    Fix Search

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
 function getMovie(){ //Gets all the movies containing the string from OMDb
-    var currentResult = document.getElementsByClassName('movieResult');
+    var currentResult = document.querySelectorAll('.movieResult');
     console.log(currentResult);
-    currentResult.forEach(element =>{
-        element.classList.add('hidden');
-    });
+
+    if(currentResult!=null){
+        for(var i=0; i<currentResult.length;i++){
+            console.log('inside FOR');
+            console.log(currentResult[i]);
+            currentResult[i].classList.add("hidden");
+            console.log(currentResult[i]);
+        }
+    }
+
     var name = document.getElementById('searchMovies').value;
     var element1 = document.getElementById('results');
-    console.log(name);
+
     fetch(`http://www.omdbapi.com/?s=${name}&apikey=b2051952`)
     .then((response)=>{
         return response.json();
@@ -22,8 +29,6 @@ function getMovie(){ //Gets all the movies containing the string from OMDb
             <img class="poster" src="https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg">
         </article> */
 
-        document.getElementById("toHide").classList.add("hidden");
-
         data.Search.forEach(element => {
             console.log(element);
             result = document.createElement('article');
@@ -38,10 +43,14 @@ function getMovie(){ //Gets all the movies containing the string from OMDb
             result.appendChild(details);
             image = document.createElement('img');
             image.className = "poster";
-            image.src=element.Poster;
+            if(element.Poster!="N/A"){
+                image.src=element.Poster;
+            }
             result.appendChild(image);
             element1.appendChild(result);
         });
     })
-    .catch("ERROR!");
+    .catch((error)=>{
+        document.getElementById("toHide").classList.remove("hidden");
+    });
 }
