ccfefd5def22eb0ee13e72c0b8663feea4d305ca
Ashish <yesashishs@gmail.com>
2021-01-07 12:16:56 -0500

    Improve Styling

--- scripts/main.js
+++ scripts/main.js
@@ -4,17 +4,17 @@ window.onload = welcome;
 //When they're about to leave, save choices as cookies if enabled
 window.onbeforeunload = saveCookies;
 
-var played = false; // Animation played
+let played = false; // Animation played
 
 //Cookie Enabled has a default true because:
 //Case 1: New User: In this case this value will be changed soon
 //Case 2: Returning user: In this case the user would be restoring previous session data so this value will have to have been true.
-var cookieEnabled = true;
+let cookieEnabled = true;
 
-var nominationList = [];
+let nominationList = [];
 
 function welcome(){
-    var title = document.getElementById("page-header");
+    let title = document.getElementById("page-header");
     title.addEventListener("animationend", aniEnd, false);
 
     if(getCookie(1)!=null){
@@ -22,7 +22,7 @@ function welcome(){
         // Restore a User's previous session:
         played=true;
 
-        var pholder = document.getElementById("placeholder");
+        let pholder = document.getElementById("placeholder");
         pholder.classList.add("hidden");
         console.log("Moving up more");
 
@@ -30,7 +30,7 @@ function welcome(){
         document.getElementById("welcome-details").classList.add("hidden");
 
         // Show Cool Headline
-        var headline = document.getElementById("page-heading");
+        let headline = document.getElementById("page-heading");
         headline.style.textShadow = "#f5f5f5 1px 0 7px";
         headline.textContent = "🏆 The 1st Annual Shoppie Awards 🏆";
 
@@ -46,10 +46,10 @@ function welcome(){
         document.getElementById("footer").classList.remove("hidden");
         console.log(document.cookie);
         
-        for(var i=1; i<=getCookie(1);i++){
+        for(let i=1; i<=getCookie(1);i++){
 
             //Update Nomination List
-            var nomination = getCookie(` ${i+1}`);
+            let nomination = getCookie(` ${i+1}`);
             nominationList.push(nomination);
 
             //Update panels
@@ -69,7 +69,7 @@ function aniEnd(event){ //When the animation is over
     }
     else{
         // document.getElementById("page-header").style.borderBottom = "1px solid #ededed";
-        var headline = document.getElementById("page-heading");
+        let headline = document.getElementById("page-heading");
         headline.style.textShadow = "#f5f5f5 1px 0 7px";
         headline.textContent = "🏆 The 1st Annual Shoppie Awards 🏆";
         main = document.getElementById("main");
@@ -100,19 +100,19 @@ function closePopup(str){
 
 //Cookies! To save your nominations for 30 days.
 function createCookie(name,value,expiry){ //Expiry in days
-    var date = new Date();
+    let date = new Date();
     date.setTime(date.getTime()+expiry*24*60*60*1000); //Converted to milliseconds
-    var expires = "expires="+date.toUTCString();
+    let expires = "expires="+date.toUTCString();
     document.cookie = name+"="+value+";"+expires;
 }
 
 function getCookie(cookiename) {
-    var cookies = document.cookie.split(';');
+    let cookies = document.cookie.split(';');
 
-    for(var i = 0; i < cookies.length; i++) {
-        var element = cookies[i].split("=");
-        var name = element[0];
-        var value = element[1];
+    for(let i = 0; i < cookies.length; i++) {
+        let element = cookies[i].split("=");
+        let name = element[0];
+        let value = element[1];
 
         if(name == cookiename){
             return value;
@@ -137,9 +137,9 @@ function infoAlert(str){
 
     popup = document.getElementById("infoAlert");
 
-    var block = document.getElementById("info-block");
+    let block = document.getElementById("info-block");
     block.innerHTML="";
-    for(var i=0; i<str.length;i++){
+    for(let i=0; i<str.length;i++){
         m1 = document.createElement("p");
         m1.classList.add("w-text");
         m1.textContent=str[i];
@@ -149,16 +149,16 @@ function infoAlert(str){
     popup.classList.remove("hidden");
 }
 
-function getMovie(){ //Gets all the movies containing the string from OMDb
+async function getMovie(){ //Gets all the movies containing the string from OMDb
     
     //The current movie results
-    var currentResult = document.querySelectorAll('.movieResult');
+    let currentResult = document.querySelectorAll('.movieResult');
     console.log(currentResult);
 
     //Removes all current results from the search
     if(currentResult!=null){
 
-        for(var i=0; i<currentResult.length;i++){
+        for(let i=0; i<currentResult.length;i++){
             console.log(currentResult[i]);
             currentResult[i].classList.add("hidden");
             console.log(currentResult[i]);
@@ -167,106 +167,113 @@ function getMovie(){ //Gets all the movies containing the string from OMDb
 
     //Hides the "Find movies by searching..."
     document.getElementById("toHide").classList.add("hidden");
+    document.getElementById("loading").classList.remove("hidden");
 
-    var name = document.getElementById('searchMovies').value;
-    var element1 = document.getElementById('results');
+    let name = document.getElementById('searchMovies').value;
+    let element1 = document.getElementById('results');
 
     //Fetch from OMDb API
-    fetch(`https://www.omdbapi.com/?s=${name}&apikey=b2051952`)
-    .then((response)=>{
-        document.getElementById("loading").classList.remove("hidden");
-        return response.json(); //Convert the response to JSON
-    })
-    .then((data)=>{
+    const response = await fetch(`https://www.omdbapi.com/?s=${name}&apikey=b2051952`)
 
-        document.getElementById("loading").classList.add("hidden");
+    const data = await response.json();
+    
+    document.getElementById("loading").classList.add("hidden");
 
-        //This is how a result will look:
-        /* <article class="movieResult card">
-                <!-- Name -->
-                <section>
-                    <p>Star Wars: A New Hope</p>
-                    <p class="p2">(1999)</p>
-                    <br>
-                    <br>
-                    <div id="nominate" class="nominate">&#9734; Nominate</div>
-                    <div id="nominated" class="nominated hidden">&#9733; Nominated!</div>
-                </section>
-                <img class="poster" src="https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg">
-            </article> */
+    //This is how a result will look:
+    /* <article class="movieResult card">
+            <!-- Name -->
+            <section>
+                <p>Star Wars: A New Hope</p>
+                <p class="p2">(1999)</p>
+                <br>
+                <br>
+                <div id="nominate" class="nominate">&#9734; Nominate</div>
+                <div id="nominated" class="nominated hidden">&#9733; Nominated!</div>
+            </section>
+            <img class="poster" src="https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg">
+        </article> */
 
-        data.Search.forEach(element => {
-            console.log(element);//for debugging
-            if(element.Type=="movie"){ // Move Awards so only movies
-                console.log(element)
-                
-                //Creates the Results
-                result = document.createElement('article');
-                result.className = "movieResult card";
-                details = document.createElement('section');
+    if (data.Response == "False"){
 
-                //The title and date
-                title = document.createElement('p');
-                title.textContent = element.Title;
-                date = document.createElement('p');
-                date.textContent = `(${element.Year})`;
-
-                details.appendChild(title);
-                details.appendChild(date);
-
-                //Breaklines between the title&date and the buttons
-                details.appendChild(document.createElement("br"));
-                details.appendChild(document.createElement("br"));
-
-                var inList = nominationList.includes(element.Title);
-
-                //The Nominate button
-                nominator1 = document.createElement("div");
-                nominator1.classList.add("nominate");
-
-                if(inList){
-                    nominator1.classList.add("hidden");
-                }
-
-                nominator1.textContent = "\u2606 Nominate";
-                nominator1.setAttribute("id",`nominate${element.Title}`);
-                nominator1.setAttribute("onclick",`nominator("${element.Title}")`);
-                details.appendChild(nominator1);
-
-                //The Nominated! button
-                nominator2 = document.createElement("div");
-                nominator2.classList.add("nominate");
-                
-                if(inList==false){
-                    nominator2.classList.add("hidden");
-                }
-
-                nominator2.textContent = "\u2605 Nominated!";
-                nominator2.setAttribute("id",`nominated${element.Title}`);
-                nominator2.setAttribute("onclick",`unnominator("${element.Title}")`);
-                details.appendChild(nominator2);
-                result.appendChild(details);
-
-                image = document.createElement('img');
-                image.className = "poster";
-
-                //The poster
-                if(element.Poster!="N/A"){ //In case a movie poster isn't available
-                    image.src=element.Poster;
-                }
-
-                result.appendChild(image);
-                element1.appendChild(result);
-            }
-        });
-    })
-    .catch((error)=>{
         document.getElementById("toHide").classList.remove("hidden");
-    });
+        document.getElementById("findMovieText").textContent = "No such movies were found!";
+
+    }else{
+        try {
+            data.Search.forEach(element => {
+                console.log(element);//for debugging
+    
+                if(element.Type=="movie"){ // Move Awards so only movies
+                    console.log(element)
+                    
+                    //Creates the Results
+                    result = document.createElement('article');
+                    result.className = "movieResult card";
+                    details = document.createElement('section');
+        
+                    //The title and date
+                    title = document.createElement('p');
+                    title.textContent = element.Title;
+                    date = document.createElement('p');
+                    date.textContent = `(${element.Year})`;
+        
+                    details.appendChild(title);
+                    details.appendChild(date);
+        
+                    //Breaklines between the title&date and the buttons
+                    details.appendChild(document.createElement("br"));
+                    details.appendChild(document.createElement("br"));
+        
+                    let inList = nominationList.includes(element.Title);
+        
+                    //The Nominate button
+                    nominator1 = document.createElement("div");
+                    nominator1.classList.add("nominate");
+        
+                    if(inList){
+                        nominator1.classList.add("hidden");
+                    }
+        
+                    nominator1.textContent = "\u2606 Nominate";
+                    nominator1.setAttribute("id",`nominate${element.Title}`);
+                    nominator1.setAttribute("onclick",`nominator("${element.Title}")`);
+                    details.appendChild(nominator1);
+        
+                    //The Nominated! button
+                    nominator2 = document.createElement("div");
+                    nominator2.classList.add("nominate");
+                    
+                    if(inList==false){
+                        nominator2.classList.add("hidden");
+                    }
+        
+                    nominator2.textContent = "\u2605 Nominated!";
+                    nominator2.setAttribute("id",`nominated${element.Title}`);
+                    nominator2.setAttribute("onclick",`unnominator("${element.Title}")`);
+                    details.appendChild(nominator2);
+                    result.appendChild(details);
+        
+                    image = document.createElement('img');
+                    image.className = "poster";
+        
+                    //The poster
+                    if(element.Poster!="N/A"){ //In case a movie poster isn't available
+                        image.src=element.Poster;
+                    }
+        
+                    result.appendChild(image);
+                    element1.appendChild(result);
+                }
+            });
+        }catch(e){
+            console.log(e);
+            document.getElementById("toHide").classList.remove("hidden");
+        }
+    }
 }
 
 function nominator(str){ //adds to nomination list and makes the nominated! button visible
-    var i=1
+    let i=1
 
     if(nominationList.length==5){
         infoAlert(["To nominate a new movie, remove one of your earlier nominations."]);
@@ -274,7 +281,6 @@ function nominator(str){ //adds to nomination list and makes the nominated! butt
     else{
         nominationList.push(str);
         console.log(nominationList);
-
 
         while(i!=6){
             element = document.getElementById(`n${i}`);
@@ -307,9 +313,15 @@ function nominator(str){ //adds to nomination list and makes the nominated! butt
     }
 }
 
+// Two Functions for un-nominating a film: 
+// One for doing it from search results and one for doing it from the Nomination list.
+// I could've used the unnominator function for both but that would mean doing a while loop when its not necessary.
+
 function unnominator(str){ //un-nominate based on the string
-    var i=1
-    while(i!=6){
+
+    let i=1
+
+    while(i!=6){ //Loops through the nomination list until it finds the movie with a matching name
 
         //Hides the Nominated! button and shows the Nominate button
         element = document.getElementById(`n${i}`);
@@ -317,14 +329,16 @@ function unnominator(str){ //un-nominate based on the string
         nominate.classList.remove("hidden");
         nominated = document.getElementById(`nominated${str}`);
         nominated.classList.add("hidden");
-        console.log(element.textContent);
 
         //Since the content will be somthing like "1. Star Wars..." we need to use include, = won't work since the result will have a number in front.
         if(element.textContent.includes(str)){
             nominationList.splice(i-1,1);
             element.textContent = `Nomination`;
             document.getElementById(`n${i}p`).classList.add("hidden");
-            i=6
+            i=6;
+        }
+        else if (element.textContent == 'Nomination') {
+            i=6; //reorder ensures that there are no gaps
         }
         else{
             i+=1;
@@ -341,7 +355,6 @@ function unnominator2(n){ //un-nominate based on position in the un-nomination l
     //If element's textContent is nomination then don't do anything because there is nothing to remove.
     if(element.textContent!="Nomination"){
 
-        console.log(element.textContent.substring(3));
         element.textContent = 'Nomination';
         document.getElementById(`n${n}p`).classList.add("hidden");
 
@@ -362,18 +375,21 @@ function unnominator2(n){ //un-nominate based on position in the un-nomination l
     reorder();
 }
 
+//Reorder Nominations when a Nomination is Removed
 function reorder(){
-    console.log("Reorder");
-    console.log(nominationList);
+
     j=1;
-    for(var i=0; i<5;i++){
+
+    for(let i=0; i<5;i++){
+
         if(i<=nominationList.length-1){
+
             element = document.getElementById(`n${i+1}`);
             document.getElementById(`n${i+1}p`).classList.remove("hidden");
+
             console.log(`${j}. ${nominationList[i]}`);
             element.textContent = `${j}. ${nominationList[i]}`;
-            console.log(element);
-            console.log(element.textContent);
+
             j=j+1;
         }
         else{
@@ -384,12 +400,28 @@ function reorder(){
 
 }
 
+//Theme Switcher
+function switchTheme(){
+    
+    let switchButton = document.getElementById("theme-switch");
+    let theme = document.getElementById("theme");
+
+    if(switchButton.checked) {
+        theme.href = "styles/dark-theme.css";
+    }else{
+        theme.href = "styles/light-theme.css";
+    }
+}
+
 //Save Cookies!
 function saveCookies(){
+    
     if(cookieEnabled && played==true){
+
         createCookie(1,nominationList.length,30); //The first cookie is the number of following cookies.
-        for(var i=0; i<nominationList.length;i++){
-            var j=i+2;
+        
+        for(let i=0; i<nominationList.length;i++){
+            let j=i+2;
             createCookie(`${j}`,nominationList[i],30);
         }
     }
